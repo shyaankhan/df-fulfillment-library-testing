571f5fcfb8d3259b89e67e4312a251c26128213e
shyaankhan <shyaankhan55@gmail.com>
2019-08-02 12:22:58 -0700

    update

--- Pepper-Fulfillment-Library.js
+++ Pepper-Fulfillment-Library.js
@@ -555,8 +555,10 @@
          let validResponses = ["BackgroundImage","BasicCard","BasicText","Carousel","CarouselNoTitles","FullScreenImage","Icons","Style","Text","TextBubbles","TriggerIntent","Video","Website"];
          for (let x = 0; x < arguments.length; x++) {
             // If simple text is passed to PepperResponse, convert it into a BasicText object before processing;
-            //if (typeof arguments[x] == "string")
-                //arguments[x] = new BasicText(arguments[x]);
+            if (typeof arguments[x] == "string"){
+                console.log("Argument[x]:"+ arguments[x]);
+                arguments[x] = new BasicText(arguments[x]);
+            }
             // Validate that the response objects are valid
             let messageType = arguments[x].constructor.name.toString();
             if ( !validResponses.includes(messageType) ) {
